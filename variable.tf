68e76bc4ec78eec2d4b579621d5d56301c3f97de
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-09-11 13:49:24 +0530

    added files

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "lifecycles" {
+  type = string
+}
+
+variable "identity_name" {
+  type = string
+}
+
+variable "resource_group_id"{
+  type = string
+}
