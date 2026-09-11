d97729c7a2ba8f2349b463a967861f9f83920f34
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-09-11 13:40:59 +0530

    added terraform

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_user_assigned_identity" "example" {
+  location            = var.location
+  name                = "${var.identity_name}-${var.lifecycles}"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_role_assignment" "roleA" {
+  scope                = var.resource_group_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_user_assigned_identity.example.principal_id
+}
+
