2a0ee46667cd7b04fc85e292a03d9942578d8fa0
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-09-13 22:36:22 +0530

    cm8

--- app/app.py
+++ app/app.py
@@ -26,4 +26,3 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.getenv("PORT", 5000))
     )
-#
--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ data "azurerm_resources" "example" {
 resource "azurerm_user_assigned_identity" "example" {
   location            = var.location
   name                = "${var.identity_name}-${var.lifecycles}"
-  resource_group_name = data.azurerm_resources.example.name
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_role_assignment" "roleA" {
-  scope                = data.azurerm_resources_group.example.id
+  scope                = data.azurerm_resource_group.example.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.example.principal_id
 }
