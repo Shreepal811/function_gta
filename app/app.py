d29ce80d0a9af6e022072bc862474c4bbf1ba4e3
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-09-13 22:04:54 +0530

    apply7

--- app/app.py
+++ app/app.py
@@ -28,4 +28,3 @@ if __name__ == "__main__":
     )# Feature Branch Change
 # Additional PR Change
 # Feature branch Change
-# Additional PR Change
--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "azurerm_resource_group" "name" {
 resource "azurerm_user_assigned_identity" "example" {
   location            = var.location
   name                = "${var.identity_name}-${var.lifecycles}"
-  resource_group_name = azurerm_resource_group.name
+  resource_group_name = azurerm_resource_group.name.name
 }
 
 resource "azurerm_role_assignment" "roleA" {
-  scope                = azurerm_resource_group.id
+  scope                = azurerm_resource_group.name.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.example.principal_id
 }
